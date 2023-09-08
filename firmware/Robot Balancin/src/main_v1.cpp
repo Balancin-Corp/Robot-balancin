9476896f9e1f151b349e987c486412419639082b
Iglesias Francisco <franviglesias@gmail.com>
2023-09-08 17:26:47 -0300

    asdasdas

--- firmware/Robot Balancin/src/main_v1.cpp
+++ firmware/Robot Balancin/src/main_v1.cpp
@@ -1,6 +1,28 @@
 #include <Arduino.h>
 #include <Wire.h>
 
+
+
+void clockwise() {
+  digitalWrite(19, LOW);
+  delayMicroseconds(10);
+  digitalWrite(18, HIGH);
+
+  digitalWrite(2, LOW);
+  delayMicroseconds(10);
+  digitalWrite(4, HIGH);
+}
+void anticlockwise() {
+  digitalWrite(18, LOW);
+  delayMicroseconds(10);
+  digitalWrite(19, HIGH);
+
+  digitalWrite(4, LOW);
+  delayMicroseconds(10);
+  digitalWrite(2, HIGH);
+}
+
+//Kalman
 float RateRoll, RatePitch, RateYaw;
 float RateCalibrationRoll, RateCalibrationPitch, RateCalibrationYaw;
 int RateCalibrationNumber;
@@ -12,27 +34,6 @@ float KalmanAnglePitch=0, KalmanUncertaintyAnglePitch=2*2;
 float Kalman1DOutput[]={0,0};
 
 float PitchOffset;
-
-void clockwise() {
-  digitalWrite(19, LOW);
-  delayMicroseconds(10);
-  digitalWrite(18, HIGH);
-
-  digitalWrite(2, LOW);
-  delayMicroseconds(10);
-  digitalWrite(4, HIGH);
-}
-
-void anticlockwise() {
-  digitalWrite(18, LOW);
-  delayMicroseconds(10);
-  digitalWrite(19, HIGH);
-
-  digitalWrite(4, LOW);
-  delayMicroseconds(10);
-  digitalWrite(2, HIGH);
-}
-
 void kalman_1d(float KalmanState, float KalmanUncertainty, float KalmanInput, float KalmanMeasurement) {
   KalmanState=KalmanState+0.004*KalmanInput;
   KalmanUncertainty=KalmanUncertainty + 0.004 * 0.004 * 4 * 4;
@@ -42,7 +43,8 @@ void kalman_1d(float KalmanState, float KalmanUncertainty, float KalmanInput, fl
   Kalman1DOutput[0]=KalmanState; 
   Kalman1DOutput[1]=KalmanUncertainty;
 }
-void gyro_signals(void) {
+
+void gyro_signals(void) { //Mide los valores raw del giroscopio
   Wire.beginTransmission(0x68);
   Wire.write(0x1A);
   Wire.write(0x05);
@@ -78,64 +80,73 @@ void gyro_signals(void) {
   AngleRoll=atan(AccY/sqrt(AccX*AccX+AccZ*AccZ))*1/(3.142/180);
   AnglePitch=-atan(AccX/sqrt(AccY*AccY+AccZ*AccZ))*1/(3.142/180);
 }
-void setup() {
 
-  pinMode(19, OUTPUT);
-  pinMode(18, OUTPUT);
-  pinMode(2, OUTPUT);
-  pinMode(4, OUTPUT);
-
-  Serial.begin(115200);
-  pinMode(13, OUTPUT);
-  digitalWrite(13, HIGH);
-  Wire.setClock(400000);
-  Wire.begin();
-  delay(250);
-  Wire.beginTransmission(0x68); 
-  Wire.write(0x6B);
-  Wire.write(0x00);
-  Wire.endTransmission();
-  for (RateCalibrationNumber=0; RateCalibrationNumber<2000; RateCalibrationNumber ++) {
+void KalmanAngle() {
     gyro_signals();
-    RateCalibrationRoll+=RateRoll;
-    RateCalibrationPitch+=RatePitch;
-    RateCalibrationYaw+=RateYaw;
-    delay(1);
-  }
-  RateCalibrationRoll/=2000;
-  RateCalibrationPitch/=2000;
-  RateCalibrationYaw/=2000;
-  LoopTimer=micros();
+    RateRoll-=RateCalibrationRoll;
+    RatePitch-=RateCalibrationPitch;
+    RateYaw-=RateCalibrationYaw;
+    kalman_1d(KalmanAngleRoll, KalmanUncertaintyAngleRoll, RateRoll, AngleRoll);
+    KalmanAngleRoll=Kalman1DOutput[0]; 
+    KalmanUncertaintyAngleRoll=Kalman1DOutput[1];
+    kalman_1d(KalmanAnglePitch, KalmanUncertaintyAnglePitch, RatePitch, AnglePitch);
+    KalmanAnglePitch=Kalman1DOutput[0]; 
+    KalmanUncertaintyAnglePitch=Kalman1DOutput[1];
+}
 
-  digitalWrite(18, LOW);
-  digitalWrite(19, LOW);
-  digitalWrite(4, LOW);
-  digitalWrite(2, LOW);
+
+void setup() {
+    pinMode(19, OUTPUT);
+    pinMode(18, OUTPUT);
+    pinMode(2, OUTPUT);
+    pinMode(4, OUTPUT);
+
+    ledcSetup(0, 30000, 8);
+    //ledcAttachPin(18, 0);
+    //ledcAttachPin(19, 0);
+
+    ledcWrite(0, 230);
+    delay(50);
+    digitalWrite(18, LOW);
+    digitalWrite(19, LOW);
+    digitalWrite(4, LOW);
+    digitalWrite(2, LOW);
+    
+
+    {
+    Serial.begin(115200);
+    pinMode(13, OUTPUT);
+    digitalWrite(13, HIGH);
+    Wire.setClock(400000);
+    Wire.begin();
+    delay(250);
+    Wire.beginTransmission(0x68); 
+    Wire.write(0x6B);
+    Wire.write(0x00);
+    Wire.endTransmission();
+    for (RateCalibrationNumber=0; RateCalibrationNumber<2000; RateCalibrationNumber ++) {
+        gyro_signals();
+        RateCalibrationRoll+=RateRoll;
+        RateCalibrationPitch+=RatePitch;
+        RateCalibrationYaw+=RateYaw;
+        delay(1);
+    }
+    RateCalibrationRoll/=2000;
+    RateCalibrationPitch/=2000;
+    RateCalibrationYaw/=2000;
+    LoopTimer=micros();
+    }
 }
 void loop() {
+    KalmanAngle();
+    float A = KalmanAnglePitch;
 
 
-  gyro_signals();
-  RateRoll-=RateCalibrationRoll;
-  RatePitch-=RateCalibrationPitch;
-  RateYaw-=RateCalibrationYaw;
-  kalman_1d(KalmanAngleRoll, KalmanUncertaintyAngleRoll, RateRoll, AngleRoll);
-  KalmanAngleRoll=Kalman1DOutput[0]; 
-  KalmanUncertaintyAngleRoll=Kalman1DOutput[1];
-  kalman_1d(KalmanAnglePitch, KalmanUncertaintyAnglePitch, RatePitch, AnglePitch);
-  KalmanAnglePitch=Kalman1DOutput[0]; 
-  KalmanUncertaintyAnglePitch=Kalman1DOutput[1];
-  Serial.print("Roll Angle [°] ");
-  Serial.print(KalmanAngleRoll);
-  Serial.print(" Pitch Angle [°] ");
-  Serial.println(KalmanAnglePitch);
-  while (micros() - LoopTimer < 4000);
-  LoopTimer=micros();
-  AnglePitch = KalmanAnglePitch;
 
-  if (AnglePitch>0) 
-    clockwise();
-  else 
-    anticlockwise();
+
+    if (A>0) 
+        clockwise();
+    else 
+        anticlockwise();
 
 }
